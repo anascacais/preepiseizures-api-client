aa48b896a5ae152d074da296025574650e301947
anascacais <anascacais@gmail.com>
2025-06-03 09:07:33 +0000

    missing import

--- examples/example_query.py
+++ examples/example_query.py
@@ -1,6 +1,6 @@
 from preepiseizures_api_client.client import PreEpiSeizuresDBClient
 
 client = PreEpiSeizuresDBClient(
-    "http://yourserver:8000", "myusername", "mypassword")
-files = client.get_files(min_events=0)
+    "http://localhost:8000", "testuser", "mypassword")
+files = client.get_files(min_events=1)
 print(files)
